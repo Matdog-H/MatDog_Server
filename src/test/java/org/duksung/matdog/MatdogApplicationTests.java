12330e2059bb71c78a8a7cf6929f1ebbfc9de772
min_jjiny <kim003512@naver.com>
2020-05-31 15:30:12 +0900

    first commit

---
+++ src/test/java/org/duksung/matdog/MatdogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.duksung.matdog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatdogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
