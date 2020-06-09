57929a56419e8b656c51886188c8886876cdbb77
KMJ <kim003512@naver.com>
2020-06-09 20:06:16 +0900

    lombok 설치

--- src/main/java/org/duksung/matdog/model/DefaultRes.java
+++ src/main/java/org/duksung/matdog/model/DefaultRes.java
@@ -1,12 +1,37 @@
 package org.duksung.matdog.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.duksung.matdog.utils.ResponseMessage;
+import org.duksung.matdog.utils.StatusCode;
 
 @Data
 @Builder
 @AllArgsConstructor
+public class DefaultRes<T> {
 
-public class DefaultRes {
+    private int status;
+
+    private String message;
+
+    private T data;
+
+    public DefaultRes(final int status, final String message) {
+        this.status = status;
+        this.message = message;
+        this.data = null;
+    }
+
+    public static<T> DefaultRes<T> res(final int status, final String message) {
+        return res(status, message, null);
+    }
+
+    public static<T> DefaultRes<T> res(final int status, final String message, final T t) {
+        return DefaultRes.<T>builder()
+                .data(t)
+                .status(status)
+                .message(message)
+                .build();
+    }
+
+    public static final DefaultRes FAIL_DEFAULT_RES = new DefaultRes(StatusCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
 }
