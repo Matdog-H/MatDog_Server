c5577740dace2659e9bd211a56dd331c443032aa
KMJ <kim003512@naver.com>
2020-10-10 13:16:24 +0900

    2020.10.10 AuthService 수정

--- src/main/java/org/duksung/matdog_server_hanuim/service/AuthService.java
+++ src/main/java/org/duksung/matdog_server_hanuim/service/AuthService.java
@@ -7,8 +7,6 @@ import org.duksung.matdog_server_hanuim.model.DefaultRes;
 import org.duksung.matdog_server_hanuim.model.LoginReq;
 import org.duksung.matdog_server_hanuim.utils.ResponseMessage;
 import org.duksung.matdog_server_hanuim.utils.StatusCode;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -37,21 +35,14 @@ public class AuthService {
         final User user_pw = userMapper.findById(loginReq.getPw());
         log.info("2");
         if (user != null) {
-            //토큰 생성
-
             final JwtService.TokenRes tokenDto = new JwtService.TokenRes(jwtService.create(user.getUserIdx()));
             return DefaultRes.res(StatusCode.OK, ResponseMessage.LOGIN_SUCCESS, tokenDto);
         } else if(user_id == null){
-            //log.info("아이디 틀림");
             return DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.NOT_FOUND_USER);
-            //return DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_USER);
         } else if(user_pw == null){
-            //log.info("비밀번호 틀림");
             return DefaultRes.res(StatusCode.UNAUTHORIZED, ResponseMessage.WRONG_PASSWORD);
         }
         log.info("들어옴");
         return DefaultRes.res(StatusCode.BAD_REQUEST, ResponseMessage.LOGIN_FAIL);
-
-
     }
 }
