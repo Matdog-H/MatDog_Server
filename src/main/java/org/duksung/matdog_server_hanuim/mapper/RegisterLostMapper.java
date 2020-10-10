e42a9448089541ea1025f458782675c4a4dc8fa3
KMJ <kim003512@naver.com>
2020-10-10 13:55:44 +0900

    2020.10.10 RegisterLostMapper 수정

--- src/main/java/org/duksung/matdog_server_hanuim/mapper/RegisterLostMapper.java
+++ src/main/java/org/duksung/matdog_server_hanuim/mapper/RegisterLostMapper.java
@@ -8,39 +8,77 @@ import java.util.List;
 
 @Mapper
 public interface RegisterLostMapper {
-    //모든 실종 공고 리스트 조회(최신순 정렬)
+    /**
+     * 모든 실종 공고 리스트 조회(최신순 정렬)
+     * @return
+     */
     @Select("SELECT * FROM register_lost ORDER BY registerIdx ASC")
     List<RegisterRes> findAll_lost();
 
-    //모든 실종 공고 리스트 조회(나이순 정렬)
+    /**
+     * 모든 실종 공고 리스트 조회(나이순 정렬)
+     * @return
+     */
     @Select("SELECT * FROM register_lost ORDER BY age DESC")
     List<RegisterRes> findAll_lost_age();
 
-    //공고 상세 화면 조회
+    /**
+     * 공고 상세 화면 조회
+     * @param registerStatus
+     * @param registerIdx
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE registerStatus = #{registerStatus} AND registerIdx = #{registerIdx}")
     Register_lost viewAllRegister_lost(@Param("registerStatus") final int registerStatus, @Param("registerIdx") final int registerIdx);
     @Select("SELECT popfile FROM dog_img_lost WHERE registerStatus = #{registerStatus} AND registerIdx = #{registerIdx}")
     dogImgUrlRes viewAllRegisterLost_img(@Param("registerStatus") final int registerStatus, @Param("registerIdx") final int registerIdx);
 
-    //검색 기능_나이
+    /**
+     * 검색 기능_나이
+     * @param keyword
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE kindCd LIKE concat('%',#{keyword},'%') OR lostPlace LIKE concat('%',#{keyword},'%') ORDER BY age DESC")
     List<RegisterRes> search_lost_age(@Param("keyword") final String keyword);
-    //검색 기능_등록일
+
+    /**
+     * 검색 기능_등록일
+     * @param keyword
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE kindCd LIKE concat('%',#{keyword},'%') OR lostPlace LIKE concat('%',#{keyword},'%') ORDER BY happenDt")
     List<RegisterRes> search_lost_date(@Param("keyword") final String keyword);
 
-    //원하는 품종 리스트 검색_나이
+    /**
+     * 원하는 품종 리스트 검색_나이
+     * @param kindCd
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE kindCd LIKE concat('%',#{kindCd},'%') ORDER BY age")
     List<RegisterRes> findDogList_lost_age(@Param("kindCd") final String kindCd);
-    //원하는 품종 리스트 검색_등록일순
+
+    /**
+     * 원하는 품종 리스트 검색_등록일순
+     * @param kindCd
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE kindCd LIKE concat('%',#{kindCd},'%') ORDER BY happenDt")
     List<RegisterRes> findDogList_lost_date(@Param("kindCd") final String kindCd);
 
-    //registerIdx 조회
+    /**
+     * registerIdx 조회
+     * @param registerIdx
+     * @return
+     */
     @Select("SELECT * FROM register_lost WHERE registerIdx = #{registerIdx}")
     Register_lost findByRegisterIdx_lost(@Param("registerIdx") final int registerIdx);
 
-    //실종 공고 등록
+    /**
+     * 실종 공고 등록
+     * @param userIdx
+     * @param re_lost
+     * @return
+     */
     @Insert("INSERT INTO register_lost(userIdx, registerStatus, kindCd, sexCd, weight, age, happenDt, lostDate,lostPlace, specialMark, careTel, email, dm, filename)" +
             "VALUES(#{userIdx}, #{re_lost.registerStatus},#{re_lost.kindCd}, #{re_lost.sexCd}, #{re_lost.weight}, #{re_lost.age}, " +
             "#{re_lost.happenDt},#{re_lost.lostDate}, #{re_lost.lostPlace},#{re_lost.specialMark}, #{re_lost.careTel}, " +
@@ -48,19 +86,35 @@ public interface RegisterLostMapper {
     @Options(useGeneratedKeys = true, keyColumn = "registerIdx", keyProperty = "re_lost.registerIdx")
     int save_lost(@Param("userIdx") final int userIdx, @Param("re_lost") final Register_lost re_lost);
 
-    //이미지 저장
+    /**
+     * 이미지 저장
+     * @param userIdx
+     * @param registerIdx
+     * @param popfile
+     * @param registerStatus
+     * @return
+     */
     @Insert("INSERT INTO dog_img_lost(userIdx, registerIdx, popfile, registerStatus) VALUES(#{userIdx}, #{registerIdx}, #{popfile}, #{registerStatus})")
     @Options(useGeneratedKeys = true, keyColumn = "dog_img.popfile")
     int save_img_lost(@Param("userIdx") final int userIdx,@Param("registerIdx") final int registerIdx, @Param("popfile") final String popfile, @Param("registerStatus") final int registerStatus);
 
-    //실종 공고 수정
+    /**
+     * 실종 공고 수정
+     * @param userIdx
+     * @param registerIdx
+     * @param register_lost
+     */
     @Update("UPDATE register_lost SET kindCd=#{register_lost.kindCd}, sexCd=#{register_lost.sexCd}, weight=#{register_lost.weight},age=#{register_lost.age}," +
             "lostDate=#{register_lost.lostDate},lostPlace=#{register_lost.lostPlace},happenDt=#{register_lost.happenDt},specialMark=#{register_lost.specialMark}, " +
             "careTel=#{register_lost.careTel}, email=#{register_lost.email},dm=#{register_lost.dm} " +
             "WHERE userIdx = #{userIdx} AND registerIdx = #{registerIdx}")
     void update_lost(@Param("userIdx") final int userIdx, @Param("registerIdx") final int registerIdx, @Param("register_lost") Register_lost register_lost);
 
-    //실종 공고 삭제
+    /**
+     * 실종 공고 삭제
+     * @param userIdx
+     * @param registerIdx
+     */
     @Delete("DELETE FROM register_lost WHERE userIdx = #{userIdx} AND registerIdx = #{registerIdx}")
     void deleteRegister_lost(@Param("userIdx") final int userIdx, @Param("registerIdx") final int registerIdx);
     @Delete("DELETE FROM dog_img_lost WHERE  userIdx = #{userIdx} AND registerIdx = #{registerIdx}")
