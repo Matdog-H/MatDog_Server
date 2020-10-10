aa7123479e7fb0b307593fc7fce432575be4cdac
KMJ <kim003512@naver.com>
2020-10-10 13:17:27 +0900

    2020.10.10 DogImgService 수정

--- src/main/java/org/duksung/matdog_server_hanuim/service/DogImgService.java
+++ src/main/java/org/duksung/matdog_server_hanuim/service/DogImgService.java
@@ -9,20 +9,15 @@ import org.duksung.matdog_server_hanuim.utils.StatusCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 
 @Service
 @Slf4j
 public class DogImgService {
-    private final DogUrlMapper dogUrlMapper;
-    private final RegisterMapper registerMapper;
     private final S3FileUploadService s3FileUploadService;
 
-    public DogImgService(DogUrlMapper dogUrlMapper, RegisterMapper registerMapper, S3FileUploadService s3FileUploadService) {
+    public DogImgService(S3FileUploadService s3FileUploadService) {
         log.info("서비스");
-        this.dogUrlMapper = dogUrlMapper;
-        this.registerMapper = registerMapper;
         this.s3FileUploadService=s3FileUploadService;
     }
 
